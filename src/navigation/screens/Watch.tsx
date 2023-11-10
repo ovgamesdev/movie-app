aea46b8abc6df6f1a16fbb8b24f0c92f328e4068
ovgamesdev <vova40227@gmail.com>
2023-11-11 02:39:28 +0600

    add hide navigation-bar in watch

--- src/navigation/screens/Watch.tsx
+++ src/navigation/screens/Watch.tsx
@@ -1,14 +1,37 @@
 import { Button } from '@components/atoms'
 import { useTheme } from '@hooks'
 import { RootStackParamList } from '@navigation'
+import { useFocusEffect } from '@react-navigation/native'
 import type { NativeStackScreenProps } from '@react-navigation/native-stack'
-import { Text, View } from 'react-native'
+import { useCallback } from 'react'
+import { AppState, StatusBar, Text, View } from 'react-native'
+import SystemNavigationBar from 'react-native-system-navigation-bar'
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Watch'>
 
 export const Watch = ({ navigation, route }: Props) => {
 	const { data } = route.params
 	const { colors } = useTheme()
+
+	useFocusEffect(
+		useCallback(() => {
+			SystemNavigationBar.navigationHide()
+			StatusBar.setHidden(true)
+
+			const subscription = AppState.addEventListener('change', nextAppState => {
+				if (nextAppState === 'active') {
+					SystemNavigationBar.navigationHide()
+					StatusBar.setHidden(true)
+				}
+			})
+
+			return () => {
+				subscription.remove()
+				SystemNavigationBar.navigationShow()
+				StatusBar.setHidden(false)
+			}
+		}, [])
+	)
 
 	return (
 		<View style={{ flex: 1, padding: 10 }}>
