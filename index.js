7fef1c6b4cc17fdf3d78f5c144ed0c51b332d46d
ovgamesdev <vova40227@gmail.com>
2023-11-18 13:44:58 +0600

    add ignore logs

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
  * @format
  */
 
-import { AppRegistry, Platform } from 'react-native'
+import { AppRegistry, LogBox, Platform } from 'react-native'
 import Config from 'react-native-config'
 import { name as appName } from './app.json'
 import App from './src/App'
 
-// LogBox.ignoreLogs(['new NativeEventEmitter'])
+LogBox.ignoreLogs(['new NativeEventEmitter'])
 if (Config.UI_MODE) {
 	Platform.constants.uiMode = Config.UI_MODE
 }
