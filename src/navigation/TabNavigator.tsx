d13019bd2a478413fa7b151300a82f9057c45ab7
ovgamesdev <vova40227@gmail.com>
2024-02-11 06:58:00 +0600

    remove tabBarHideOnKeyboard, fix imdb search & add watch button

--- src/navigation/TabNavigator.tsx
+++ src/navigation/TabNavigator.tsx
@@ -20,7 +20,7 @@ export const TabNavigator = () => {
 				headerShown: false,
 				// tabBarStyle: isLandscape ? { display: 'none' } : {},
 				tabBarShowLabel: false,
-				tabBarHideOnKeyboard: true,
+				// tabBarHideOnKeyboard: true,
 
 				// tabBarBackground: () => <View style={{ backgroundColor: 'red', position: 'absolute', top: 0, right: 0, bottom: 0, left: 0 }}></View>,
 				tabBarButton: ({ children, onPress }) => (
--- src/navigation/screens/Search.tsx
+++ src/navigation/screens/Search.tsx
@@ -1,4 +1,4 @@
-import { ActivityIndicator, Input, InputType } from '@components/atoms'
+import { ActivityIndicator, Button, Input, InputType } from '@components/atoms'
 import { SearchHistory, SearchResults } from '@components/organisms'
 import { useDebounce, useNavigation, useTypedDispatch } from '@hooks'
 import { HomeTabParamList } from '@navigation'
@@ -9,6 +9,8 @@ import { FC, useEffect, useRef, useState } from 'react'
 import { KeyboardAvoidingView, NativeSyntheticEvent, ScrollView, TVFocusGuideView, Text, TextInputSubmitEditingEventData, ToastAndroid, View } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { createStyleSheet, useStyles } from 'react-native-unistyles'
+
+const isImdbRegex = /^(tt\d{4,9})/
 
 type Props = NativeStackScreenProps<HomeTabParamList, 'Search'>
 
@@ -21,47 +23,50 @@ export const Search: FC<Props> = ({ route }) => {
 	const dispatch = useTypedDispatch()
 
 	const [keyword, setKeyword] = useState('')
+	const isImdbSearch = !!isImdbRegex.exec(keyword)
 	const deferredKeyword = useDebounce(keyword, 300)
-	const { isFetching, data } = useGetSuggestSearchQuery({ keyword: deferredKeyword }, { skip: deferredKeyword.length === 0 })
-	const isEmpty = !data?.topResult?.global && !(data?.movies && data.movies.length > 0) && !(data?.movieLists && data.movieLists.length > 0) && !(data?.persons && data.persons.length > 0)
-	const isLoading = keyword !== deferredKeyword || isFetching
+	const { isFetching, data } = useGetSuggestSearchQuery({ keyword: deferredKeyword }, { skip: isImdbSearch || deferredKeyword.length === 0 })
+	const isEmpty = isImdbSearch ? false : !data?.topResult?.global && !(data?.movies && data.movies.length > 0) && !(data?.movieLists && data.movieLists.length > 0) && !(data?.persons && data.persons.length > 0)
+	const isLoading = isImdbSearch || deferredKeyword.length === 0 ? false : isFetching || keyword !== deferredKeyword
 
 	const ref = useRef<InputType>(null)
 
 	useEffect(() => navigation.addListener('focus', () => setTimeout(() => keyword.length === 0 && ref.current?.focus(), 0)), [navigation, ref, keyword])
 
-	// isId: t => !(!t.match(/^(\d{3,9})$/) && !t.match(/^(tt\d{4,9})$/))
-
 	const onSubmitEditing = async ({ nativeEvent: { text } }: NativeSyntheticEvent<TextInputSubmitEditingEventData>) => {
-		const isNotId = !/^(tt\d{4,9})$/.exec(text) // !/^(\d{3,9})$/.exec(text) &&
+		const isNotId = !isImdbRegex.exec(text)
 		if (isNotId) return
 
 		if (text.startsWith('tt')) {
-			const { data } = await dispatch(themoviedbApi.endpoints.getMovieById.initiate({ id: text }))
+			watchImdb(text)
+		}
+	}
 
-			if (data) {
-				navigation.navigate('Watch', {
-					data: {
-						id: text as `tt${number}`,
-						poster: data.poster_path ? getTMDBPosterImage(data.poster_path) : null,
-						provider: null,
+	const watchImdb = async (id: string) => {
+		const { data } = await dispatch(themoviedbApi.endpoints.getMovieById.initiate({ id }))
 
-						...(data.media_type === 'tv'
-							? {
-									title: data.name,
-									type: 'TvSeries',
-									year: Number(data.first_air_date.slice(0, 4)) || null
-							  }
-							: {
-									title: data.title,
-									type: 'Film',
-									year: Number(data.release_date.slice(0, 4)) || null
-							  })
-					}
-				})
-			} else {
-				ToastAndroid.show('IMDB: Не удалось найти фильм', ToastAndroid.SHORT)
-			}
+		if (data) {
+			navigation.navigate('Watch', {
+				data: {
+					id: id as `tt${number}`,
+					poster: data.poster_path ? getTMDBPosterImage(data.poster_path) : null,
+					provider: null,
+
+					...(data.media_type === 'tv'
+						? {
+								title: data.name,
+								type: 'TvSeries',
+								year: Number(data.first_air_date.slice(0, 4)) || null
+						  }
+						: {
+								title: data.title,
+								type: 'Film',
+								year: Number(data.release_date.slice(0, 4)) || null
+						  })
+				}
+			})
+		} else {
+			ToastAndroid.show('IMDB: Не удалось найти фильм', ToastAndroid.SHORT)
 		}
 	}
 
@@ -72,7 +77,13 @@ export const Search: FC<Props> = ({ route }) => {
 			</View>
 
 			<KeyboardAvoidingView behavior='padding' style={{ flex: 1 }}>
-				{keyword.length === 0 ? (
+				{isImdbSearch ? (
+					<Button onPress={async () => watchImdb(deferredKeyword)} paddingHorizontal={16} paddingVertical={11} animation='scale' transparent alignItems='stretch' flexDirection='row'>
+						<Text numberOfLines={2} style={styles.watch}>
+							Смотреть как IMDB
+						</Text>
+					</Button>
+				) : keyword.length === 0 ? (
 					<SearchHistory />
 				) : isLoading ? (
 					<View style={styles.emptyContainer}>
@@ -82,11 +93,11 @@ export const Search: FC<Props> = ({ route }) => {
 					<View style={styles.emptyContainer}>
 						<Text style={styles.emptyText}>По вашему запросу ничего не найдено</Text>
 					</View>
-				) : (
+				) : data ? (
 					<ScrollView contentContainerStyle={[styles.resultsContainer, { paddingBottom: 15 + insets.bottom }]}>
 						<SearchResults data={data} />
 					</ScrollView>
-				)}
+				) : null}
 			</KeyboardAvoidingView>
 		</TVFocusGuideView>
 	)
@@ -112,5 +123,9 @@ const stylesheet = createStyleSheet(theme => ({
 		fontSize: 15,
 		paddingHorizontal: 30,
 		textAlign: 'center'
+	},
+	watch: {
+		color: theme.colors.text100,
+		fontSize: 15
 	}
 }))
